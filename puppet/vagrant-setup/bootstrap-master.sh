3e9293f1ce60366a4ec24ac6ecf32060e196d36c
spicy <spicy@test.org>
2020-05-06 11:59:07 -0700

    basic vagrant setup

---
+++ puppet/vagrant-setup/bootstrap-master.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Run on VM to bootstrap Puppet Master server
+
+if ps aux | grep "puppet master" | grep -v grep 2> /dev/null
+then
+    echo "Puppet Master is already installed. Exiting..."
+else
+    # Install Puppet Master
+    wget http://apt.puppetlabs.com/puppet5-release-xenial.deb && \
+    sudo dpkg -i puppet5-release-xenial.deb && \
+    sudo apt-get update -yq && sudo apt-get upgrade -yq && \
+    sudo apt-get install -yq puppetmaster
+
+    # Configure /etc/hosts file
+    echo "" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "# Host config for Puppet Master and Agent Nodes" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "192.168.32.5    puppet.example.com  puppet" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "192.168.32.10   node01.example.com  node01" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "192.168.32.20   node02.example.com  node02" | sudo tee --append /etc/hosts 2> /dev/null
+
+    # Add optional alternate DNS names to /etc/puppet/puppet.conf
+    sudo sed -i 's/.*\[main\].*/&\ndns_alt_names = puppet,puppet.example.com/' /etc/puppet/puppet.conf
+
+    # Install some initial puppet modules on Puppet Master server
+    sudo puppet module install puppetlabs-ntp
+    sudo puppet module install puppetlabs-docker
+    sudo puppet module install puppetlabs-git
+    sudo puppet module install puppetlabs-vcsrepo
+    sudo puppet module install garystafford-fig
+    sudo puppet module install puppetlabs-kubernetes
+
+    # symlink manifest from Vagrant synced folder location
+    #ln -s /vagrant/site.pp /etc/puppet/manifests/site.pp
+fi
---
+++ puppet/vagrant-setup/bootstrap-node.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Run on VM to bootstrap Puppet Agent nodes
+
+if ps aux | grep "puppet agent" | grep -v grep 2> /dev/null
+then
+    echo "Puppet Agent is already installed. Moving on..."
+else
+    wget -P /tmp/ http://apt.puppetlabs.com/puppet5-release-xenial.deb
+    dpkg -i /tmp/puppet5-release-xenial.deb
+    apt-get update    
+    sudo apt-get install -yq puppet
+fi
+
+if cat /etc/crontab | grep puppet 2> /dev/null
+then
+    echo "Puppet Agent is already configured. Exiting..."
+else
+    sudo apt-get update -yq && sudo apt-get upgrade -yq
+
+   # sudo puppet resource cron puppet-agent ensure=present user=root minute=30 \
+     #   command='/usr/bin/puppet agent --onetime --no-daemonize --splay'
+
+    #sudo puppet resource service puppet ensure=running enable=true
+
+    # Configure /etc/hosts file
+    echo "" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "# Host config for Puppet Master and Agent Nodes" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "192.168.32.5    puppet.example.com  puppet" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "192.168.32.10   node01.example.com  node01" | sudo tee --append /etc/hosts 2> /dev/null && \
+    echo "192.168.32.20   node02.example.com  node02" | sudo tee --append /etc/hosts 2> /dev/null
+
+    # Add agent section to /etc/puppet/puppet.conf
+    echo "" && echo "[agent]\nserver=puppet" | sudo tee --append /etc/puppet/puppet.conf 2> /dev/null
+
+    sudo puppet agent --enable
+fi
